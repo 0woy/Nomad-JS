2acb88e35087972125685b139545debaa106d7eb
0woy <qkrdbsdk1030@naver.com>
2022-02-09 16:33:27 +0900

    click event upgrade
    if you click, change the color.(blue or tomato)

--- main.js
+++ main.js
@@ -1,47 +1,20 @@
 'use strict';
 
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
+// click -> blue -> click -> tomato
 function handleTitleClick(){
-    title.innerText ="Grab me!"
-    title.style.color ="blue";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor="tomato";
+    }
+    else{
+        newColor="blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    title.innerText ="Mouse is here!";
-}
-
-function handleMouseLeave(){
-    title.innerText ="Mouse is gone!";
-}
+h1.addEventListener("click", handleTitleClick);
 
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter",handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-//Window Event
-
-//1. 화면 크기 조절시 발생하는 event
-function handleWindowResize(){
-    document.body.style.backgroundColor="tomato";
-}
-window.addEventListener("resize", handleWindowResize);
-
-
-//2. copy event
-function handleWindowCopy(){
-    alert("copier!");
-}
-window.addEventListener("copy",handleWindowCopy);
-
-// 3. Online, Offline event
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("All goood");
-}
-window.addEventListener("offline",handleWindowOffline);
-window.addEventListener("online",handleWindowOnline);
