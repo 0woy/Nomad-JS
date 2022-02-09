26d10b393f3976104eadfd8d5e3a03a6bca19a11
0woy <qkrdbsdk1030@naver.com>
2022-02-09 16:24:33 +0900

    update Window Event
    added resize, copy, online, offline event listener

--- main.js
+++ main.js
@@ -15,15 +15,33 @@ function handleMouseLeave(){
     title.innerText ="Mouse is gone!";
 }
 
-//Event를 Listen하는 2가지 방법
-title.onclick = handleTitleClick;
+
+title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter",handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
 
+//Window Event
 
-//window Event
+//1. 화면 크기 조절시 발생하는 event
 function handleWindowResize(){
     document.body.style.backgroundColor="tomato";
 }
-
 window.addEventListener("resize", handleWindowResize);
+
+
+//2. copy event
+function handleWindowCopy(){
+    alert("copier!");
+}
+window.addEventListener("copy",handleWindowCopy);
+
+// 3. Online, Offline event
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("All goood");
+}
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
