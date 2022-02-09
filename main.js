02010dc11301551f4601fd5b805731278c945938
0woy <qkrdbsdk1030@naver.com>
2022-02-09 19:10:24 +0900

    learn about preventdefault 2
    link click시 browser 기본 행동 방지

--- main.js
+++ main.js
@@ -4,13 +4,23 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 // userName check
 function onLoginSubmit(event){
     // preventDefault: evnet의 기본행동 방지
     event.preventDefault();
-    console.log(loginInput.value);
+    console.dir(loginInput.value);
 }
 
 // submit event -> enter, button
 loginForm.addEventListener("submit", onLoginSubmit);
 
+// alert는 browser의 다른 동작을 막음, but ok를 누르는 순간 browser는 기본동작을 수행하게 됨.
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event);
+}
+link.addEventListener("click", handleLinkClick);
+
+// handleLinkClick({information about the event that just happened})
