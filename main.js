66c10d1458a36deac4f54774ba553b0d2da5edc0
0woy <qkrdbsdk1030@naver.com>
2022-02-09 16:01:53 +0900

    learn about event
    click, mouseenter, mouseleave

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const title = document.querySelector(".hello h1");
+
+function handleTitleClick(){
+    title.innerText ="Grab me!"
+    title.style.color ="blue";
+}
+
+function handleMouseEnter(){
+    title.innerText ="Mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText ="Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
