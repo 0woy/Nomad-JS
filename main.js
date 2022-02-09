207e7006e4b37940edb9903cc6d54cb635b8789f
0woy <qkrdbsdk1030@naver.com>
2022-02-09 17:03:32 +0900

    Learn about toggle
    Using toggle->color can be changed only writing 1 line.

--- main.js
+++ main.js
@@ -2,16 +2,11 @@
 
 const h1 = document.querySelector(".hello h1");
 
-// with Css, change color
-// ClassList : NOT replace className only add clicked class.
+
+//toggle: add and remove 
+// Not repeat string, we can use string -> cicked.
 function handleTitleClick(){
-    const clickedClass = "clicked";
-    if(h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    }
-    else{
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
 }
  
 h1.addEventListener("click", handleTitleClick);
