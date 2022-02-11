6cb64f882de3e3bc2f0cb636ac24ea35655a5eb3
0woy <qkrdbsdk1030@naver.com>
2022-02-11 17:58:16 +0900

    Getting username
    after Entering, hidden form and make greeting

--- main.js
+++ main.js
@@ -3,24 +3,22 @@
 // HTML에서 element 끌어오기
 const loginForm = document.querySelector("#login-form")
 const loginInput = loginForm.querySelector("input")
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME="hidden";
 
-// userName check
 function onLoginSubmit(event){
-    // preventDefault: evnet의 기본행동 방지
     event.preventDefault();
-    console.dir(loginInput.value);
+    const username = loginInput.value;
+    
+    // login 후 form -> hidden
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText=`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
 } 
 
 // submit event -> enter, button
 loginForm.addEventListener("submit", onLoginSubmit);
-
-// alert는 browser의 다른 동작을 막음, but ok를 누르는 순간 browser는 기본동작을 수행하게 됨.
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
-}
-link.addEventListener("click", handleLinkClick);
 
 // handleLinkClick({information about the event that just happened})
